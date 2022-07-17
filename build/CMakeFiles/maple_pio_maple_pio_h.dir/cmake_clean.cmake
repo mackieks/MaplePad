e13298710028cb2ab7bc86ec7317438df521cf05
Mackie Kannard-Smith <floralyveltalgriffin@gmail.com>
2022-07-17 12:24:33 -0400

    Remove build/

--- build/CMakeFiles/maple_pio_maple_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/maple_pio_maple_pio_h"
-  "maple.pio.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/maple_pio_maple_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/maple_pio_maple_pio_h.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for maple_pio_maple_pio_h.
-# This may be replaced when dependencies are built.
